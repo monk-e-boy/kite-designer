15d9ba6e127950bebe050c34a79b096597255242
monk-e-boy <jwglazebrook@gmail.com>
2020-07-18 00:59:23 +0100

    LE hacks
    the start of the LE build

--- LE.gd
+++ LE.gd
@@ -1,23 +1,40 @@
-extends Node
+extends MeshInstance
 
+export(SpatialMaterial) var material
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-var tube_angle = 10
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	var tube_scene = load("res://LeadingEdgeSection.tscn")
-	var tube = tube_scene.instance()
-	tube.transform.origin = Vector3(0,0,0)
-	tube.translate_object_local(Vector3(0,1,0))
-	self.add_child(tube)
+	var surface_tool = SurfaceTool.new()
+	var mesh = Mesh.new()
+	surface_tool.begin(Mesh.PRIMITIVE_LINES)
+	surface_tool.set_material(material)
+	
+	build(surface_tool)
+	
+	surface_tool.generate_normals()
+	surface_tool.commit(mesh)
+	self.set_mesh(mesh)
 
-	var tube2 = tube_scene.instance()
-	tube2.transform.origin = Vector3(0,0,0)
-	tube2.translate_object_local(Vector3(1,0,0))
-	self.add_child(tube2)
+func build(surface_tool):
+	tube_end(surface_tool, Vector3(0,0,0), 0, 1)
+
+func tube_end(surface_tool, tube_end: Vector3, tube_angle_z: float, radius: float):
+	var p = Vector3(0,radius,0)
+	var count = 20
+	var angle = 360.0 / count
+	
+	var points = []
+	for i in range(count):
+		points.append(p.rotated(Vector3(1,0,0), deg2rad(angle*i)))
+		
+	for i in range(points.size()):
+		points[i] = points[i].rotated(Vector3(0,0,1), deg2rad(tube_angle_z))
+	
+	for i in range(points.size()-1):
+		surface_tool.add_vertex(tube_end+points[i])
+		surface_tool.add_vertex(tube_end+points[i+1])
+		
+	surface_tool.add_vertex(tube_end+points[points.size()-1])
+	surface_tool.add_vertex(tube_end+points[0])
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
